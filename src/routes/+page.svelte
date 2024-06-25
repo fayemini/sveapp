bd5185f03c3d586116d828284fba81baf67c0978
Faye Valentine <aquiverse@protonmail.com>
2024-06-25 08:01:42 +0000

    use static adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+</nav>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
